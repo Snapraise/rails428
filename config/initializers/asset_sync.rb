ba01f0dc177bf1e0821b89d883e3c6203880d0af
Dmitry Polyakovsky <dmitrypol@gmail.com>
2017-05-10 11:14:12 -0700

    aws creds tweak for asset_sync

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,6 +1,6 @@
 AssetSync.configure do |config|
   config.fog_provider = 'AWS'
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  config.aws_access_key_id = ENV['AWS_ACCESS_KEY']
   config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
   # To use AWS reduced redundancy storage.
   # config.aws_reduced_redundancy = true
